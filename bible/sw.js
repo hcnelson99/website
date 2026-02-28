e3097162fb35f05e9ea1ae190294abe69d9d7015
Henry Nelson <hcnelson99@gmail.com>
2026-02-28 15:37:34 -0500

    .

--- bible/sw.js
+++ bible/sw.js
@@ -1,31 +1,17 @@
 // @ts-check
 const CACHE = 'bible-v1';
 
-const BOOK_IDS = [
-  'genesis', 'exodus', 'leviticus', 'numbers', 'deuteronomy', 'joshua',
-  'judges', 'ruth', '1samuel', '2samuel', '1kings', '2kings', '1chronicles',
-  '2chronicles', 'ezra', 'nehemiah', 'esther', 'job', 'psalms', 'proverbs',
-  'ecclesiastes', 'songofsolomon', 'isaiah', 'jeremiah', 'lamentations',
-  'ezekiel', 'daniel', 'hosea', 'joel', 'amos', 'obadiah', 'jonah', 'micah',
-  'nahum', 'habakkuk', 'zephaniah', 'haggai', 'zechariah', 'malachi',
-  'matthew', 'mark', 'luke', 'john', 'acts', 'romans', '1corinthians',
-  '2corinthians', 'galatians', 'ephesians', 'philippians', 'colossians',
-  '1thessalonians', '2thessalonians', '1timothy', '2timothy', 'titus',
-  'philemon', 'hebrews', 'james', '1peter', '2peter', '1john', '2john',
-  '3john', 'jude', 'revelation',
-];
-
-const ALL_FILES = [
-  './',
-  './index.html',
-  './app.js',
-  '../ui.js',
-  './data/books.json',
-  ...BOOK_IDS.map(id => `./data/${id}.txt`),
-];
+const APP_FILES = ['./', './index.html', './app.js', '../ui.js', './data/books.json'];
 
 self.addEventListener('install', (/** @type {any} */ e) => {
-  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ALL_FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(async cache => {
+      await cache.addAll(APP_FILES);
+      const res = /** @type {Response} */ (await cache.match('./data/books.json'));
+      const books = /** @type {{ id: string }[]} */ (await res.json());
+      await cache.addAll(books.map(b => `./data/${b.id}.txt`));
+    })
+  );
   /** @type {any} */ (self).skipWaiting();
 });
 
